87837464c502f395453381419a840d5f31f8802d
anlal <anlal@ANL>
2024-12-09 22:15:05 +0300

    Add project files.

---
+++ MyApiNight.DataAccessLayer/EntitiyFramework/EfProductDal.cs
@@ -0,0 +1,19 @@
+﻿using MyApiNight.DataAccessLayer.Abstract;
+using MyApiNight.DataAccessLayer.Context;
+using MyApiNight.DataAccessLayer.Repositories;
+using MyApiNight.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApiNight.DataAccessLayer.EntitiyFramework
+{
+    public class EfProductDal : GenericRepositories<Product>, IProductDal
+    {
+        public EfProductDal(ApiContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ MyApiNight.DataAccessLayer/Repositories/GenericRepositories.cs
@@ -0,0 +1,49 @@
+﻿using MyApiNight.DataAccessLayer.Abstract;
+using MyApiNight.DataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApiNight.DataAccessLayer.Repositories
+{
+    public class GenericRepositories<T> : IGenericDal<T> where T : class
+    {
+        private readonly ApiContext _context;
+
+        public GenericRepositories(ApiContext context)
+        {
+            _context = context;
+        }
+
+        public void Delete(int id)
+        {
+            var value = _context.Set<T>().Find(id);
+            _context.Set<T>().Remove(value);
+            _context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public void Insert(T entity)
+        {
+            _context.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
